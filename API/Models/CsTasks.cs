895886e99d1ba47be5e554ef919c174e88cfcfca
rizaleff <rizaleffendi92@gmail.com>
2023-10-23 11:26:27 +0700

    Squashed commit of the following:
    commit 37eff7c9788f8fec8872a47670e4a5572177849d
    Author: indrajanwars <indrajanwarsetyadi@gmail.com>
    Date:   Mon Oct 23 10:56:28 2023 +0700
    
        Small Update & Change Name
    
    commit fa61e5d868e8337c73dbe273c365ca8eb5cd2ce0
    Merge: 9c28b66 7de1d3a
    Author: indrajanwars <indrajanwarsetyadi@gmail.com>
    Date:   Mon Oct 23 10:40:23 2023 +0700
    
        Merge branch 'indra'
    
    commit 9c28b66be9a4e841a5f03291b8180d09c0ca5a28
    Merge: b0e9158 a32c15f
    Author: indrajanwars <indrajanwarsetyadi@gmail.com>
    Date:   Mon Oct 23 10:39:59 2023 +0700
    
        Merge branch 'rizal'
    
    commit 7de1d3a3642786b5830aca837d405e8d32765bbe
    Author: indrajanwars <indrajanwarsetyadi@gmail.com>
    Date:   Mon Oct 23 10:29:29 2023 +0700
    
        Create CsTasks.cs

---
+++ API/Models/CsTasks.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_cs_tasks")]
+public class CsTasks : BaseEntity
+{
+    [Column("cs_guid", TypeName = "uniqueidentifier")]
+    public Guid CsGuid { get; set; }
+
+    [Column("task_guid, TypeName = uniqueidentifier")]
+    public Guid TaskGuid { get; set; }
+
+    // Cardinality
+    public Employees? Employee { get; set; }
+    public Tasks? Task { get; set; }
+}
--- API/Models/Reports.cs
+++ API/Models/Reports.cs
@@ -3,16 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Models;
 [Table("tb_reports")]
-public class Report : BaseEntity
+public class Reports : BaseEntity
 {
     [Column("title", TypeName = "nvarchar(100)")]
     public string Title { get; set; }
+
     [Column("description", TypeName = "nvarchar(max)")]
     public string Description{ get; set; }
+
     [Column("employee_guid")]
     public Guid EmployeeGuid { get; set; }
+
     [Column("gender")]
-    public StatusLevel Status { get; set }
+    public StatusLevel Status { get; set; }
+
     [Column("photo_url", TypeName = "nvarchar(max)")]
     public string PhotoUrl { get; set; }
 }
