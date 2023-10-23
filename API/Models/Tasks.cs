aa0726e32274bf970b66cdf5c18fb392330fa616
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-10-23 10:12:35 +0700

    Create Tasks.cs

---
+++ API/Models/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_tasks")]
+public class Tasks : BaseEntity
+{
+    [Column("title", TypeName = "nvarchar(100)")]
+    public string Title { get; set; }
+
+    [Column("description", TypeName = "nvarchar(max)")]
+    public string Description { get; set; }
+
+    [Column("task_estimate")]
+    public DateTime TaskEstimate { get; set; }
+
+    [Column("is_approved")]
+    public bool IsApproved { get; set; }
+
+    // Cardinality
+    public Reports? Report { get; set; }
+    public TaskReports? TaskReport { get; set; }
+    public CsTasks? CsTask { get; set; }
+}
