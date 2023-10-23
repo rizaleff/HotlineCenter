3b2049078272cd61d194d34f92f5309f6703d2a2
rizaleff <rizaleffendi92@gmail.com>
2023-10-23 10:09:47 +0700

    Update Models

---
+++ API/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+public class Project : BaseEntity
+{
+    [Column("report_guid")]
+    public Guid ReportGuid { get; set; }
+    [Column("title", TypeName = "nvarchar(100)")]
+    public string Title { get; set; }
+    [Column("description", TypeName = "nvarchar(max)")]
+    public string Description { get; set; }
+    [Column("is_approve")]
+    public bool IsApprove { get; set; }
+    [Column("budget")]
+    public float Budget { get; set; }
+}
---
+++ API/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_roles")]
+public class Role : GeneralModel
+{
+    [Column("name", TypeName = "nvarchar(100)")]
+    public string Name { get; set; }
+
+}
