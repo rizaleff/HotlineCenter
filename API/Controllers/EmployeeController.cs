30b158cba8ad68f3651baa436c0f6fe6834b97af
rizaleff <rizaleffendi92@gmail.com>
2023-10-24 16:59:09 +0700

    CRUD Employee

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -1,5 +1,9 @@
 ﻿using API.Contracts;
+using API.Dtos.Employees;
+using API.Models;
+using API.Utilities.Handlers;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace API.Controllers;
 
@@ -14,44 +18,143 @@ public class EmployeeController : ControllerBase
         _employeeRepository = employeeRepository;
     }
 
-    [HttpGet("details")]
+   /* [HttpGet("details")]
     public IActionResult GetDetails()
     {
         
     }
-
+*/
 
     [HttpGet]
     public IActionResult GetAll()
     {
-        
+        var result = _employeeRepository.GetAll();
+        if (!result.Any())
+        {
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound, 
+                Status = HttpStatusCode.NotFound.ToString(), 
+                Message = "Data Not Found"
+            });
+        }
+        var data = result.Select(x => (EmployeeDto)x);
+        return Ok(new ResponseOKHandler<IEnumerable<EmployeeDto>>(data));
     }
 
 
     [HttpGet("{guid}")]
     public IActionResult GetByGuid(Guid guid)
     {
-        
+        var result = _employeeRepository.GetByGuid(guid);
+        if (result is null)
+        {
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound, 
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "Data Not Found" 
+            });
+        }
+        return Ok(new ResponseOKHandler<EmployeeDto>((EmployeeDto)result));
     }
 
 
     [HttpPost]
     public IActionResult Create(CreateEmployeeDto employeeDto)
     {
-        
+        try
+        {
+            Employee toCreate = employeeDto;
+            toCreate.Nik = GenerateHandler.GenerateNik(_employeeRepository.GetLastNik());
+            var result = _employeeRepository.Create(toCreate);
+            return Ok(new ResponseOKHandler<EmployeeDto>((EmployeeDto)result));
+        }
+        catch (ExceptionHandler ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(), 
+                Message = "Failed to Create Data", 
+                Error = ex.Message  
+            });
+        }
     }
 
 
     [HttpPut]
     public IActionResult Update(EmployeeDto employeeDto)
     {
-        
+        try
+        {
+            var employeeByGuid = _employeeRepository.GetByGuid(employeeDto.Guid);
+
+            if (employeeByGuid is null)
+            {
+               
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound, 
+                    Status = HttpStatusCode.NotFound.ToString(), 
+                    Message = "Data Not Found"
+                });
+            }
+
+            Employee toUpdate = employeeDto;
+            toUpdate.Nik = employeeByGuid.Nik;
+
+           
+            toUpdate.CreatedDate = employeeByGuid.CreatedDate;
+
+            _employeeRepository.Update(toUpdate);
+
+            return Ok(new ResponseOKHandler<string>("Data Updated"));
+        }
+        catch (ExceptionHandler ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError, 
+                Status = HttpStatusCode.InternalServerError.ToString(), 
+                Message = "Failed to create data",
+                Error = ex.Message 
+            });
+        }
     }
 
 
     [HttpDelete("{guid}")]
     public IActionResult Delete(Guid guid)
     {
-        
+        try
+        {
+            
+            var employeeByGuid = _employeeRepository.GetByGuid(guid);
+
+            if (employeeByGuid is null)
+            {
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound, 
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Data Not Found"
+                });
+            }
+
+            _employeeRepository.Delete(employeeByGuid);
+
+            return Ok(new ResponseOKHandler<string>("Data Deleted"));
+        }
+        catch (ExceptionHandler ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(), 
+                Message = "Failed to create data", 
+                Error = ex.Message
+            });
+        }
     }
 }
--- API/Repositories/EmployeeRepository.cs
+++ API/Repositories/EmployeeRepository.cs
@@ -6,5 +6,9 @@ namespace API.Repositories;
 public class EmployeeRepository : GeneralRepository<Employee>, IEmployeeRepository
 {
     public EmployeeRepository(HotlineCenterDbContext context) : base(context) { }
-
+    public string GetLastNik()
+    {
+        Employee? employee = _context.Employees.OrderByDescending(e => e.Nik).FirstOrDefault();
+        return employee?.Nik ?? "";
+    }
 }
