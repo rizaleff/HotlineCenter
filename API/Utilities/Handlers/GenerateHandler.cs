b47a39ca14815bd0bcba4e89eac15dd86955f2d1
rizaleff <rizaleffendi92@gmail.com>
2023-10-24 17:00:54 +0700

    Employee CRUD

---
+++ API/Utilities/Handlers/GenerateHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Utilities.Handlers;
+public class GenerateHandler
+{
+    public static string GenerateNik(string lastNik)
+    {
+        if (lastNik == "") return "111111";
+        int nik = Convert.ToInt32(lastNik);
+        nik += 1;
+        return nik.ToString();
+    }
+
+    public static int GenerateOtp()
+    {
+        Random random = new Random();
+        int otp = random.Next(100000, 999999);
+        return otp;
+    }
+}
