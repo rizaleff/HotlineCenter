c25e462b91493e66a94d12df7efb69b683f601cd
NandaShalihin <nandashalihin10@gmail.com>
2023-10-25 13:09:38 +0700

    halaman create report, details report

---
+++ Client/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create() 
+        {
+            return View("Create");
+        }
+
+        public IActionResult Details()
+        {
+            return View("Details");
+        }
+    }
+}
