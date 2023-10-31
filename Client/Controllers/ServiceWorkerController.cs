20d8c37d94d122a4fa14900152e59ae94b19acd2
NandaShalihin <nandashalihin10@gmail.com>
2023-10-31 17:39:37 +0700

    Update

---
+++ Client/Controllers/ServiceWorkerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class ServiceWorkerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Dashboard");
+        }
+
+        
+    }
+}
